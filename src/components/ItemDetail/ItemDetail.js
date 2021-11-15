1c75d44631b2d5305e6bbbb1bad5d0168def2438
e-chevalier <echevalier.dev@gmail.com>
2021-11-15 10:24:44 -0300

    Clase 9 v1

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ItemCount from '../ItemCount/ItemCount'
+import { Link } from 'react-router-dom'
 import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
 import Row from 'react-bootstrap/Row'
+import Button from 'react-bootstrap/Button'
+
+
 
 const ItemDetail = ({ product }) => {
+
+    const [qtyToAdd, setQtyToAdd] = useState(0);
+    const [wasClicked, setWasClicked] = useState(false);
+
+    const onAdd = (newValue) => {
+        setWasClicked(true);
+        setQtyToAdd(newValue);
+    }
+
+    console.log("QtyIs: " + qtyToAdd );
+
     return (
         <Card className="my-5">
             <Row className="g-0">
@@ -15,6 +31,9 @@ const ItemDetail = ({ product }) => {
                         <Card.Title className="fs-2">{product.name}</Card.Title>
                         <Card.Text>Precio por Kg  <span className="fw-bold fs-4">${product.price}</span></Card.Text>
                         <Card.Text className="my-5 text-start">{product.description}</Card.Text>
+                        {wasClicked ?
+                        <Button as={Link} to={'/cart'} variant="success">Terminar Compra</Button>:
+                        <ItemCount prodId={product.id} stock={product.stock} initial={0} onAdd={onAdd} />}
                     </Card.Body>
                 </Col>
             </Row>
